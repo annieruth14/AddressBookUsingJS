82963f2bbfbf0fcbf7e508ad45f50ca8d70fd2b4
annieruth14 <ruthannie3@gmail.com>
2020-11-16 19:51:43 +0530

    [Annie]Added: code to create AddressBook Contact

--- AddressBook.js
+++ AddressBook.js
@@ -1,1 +1,14 @@
 console.log("Welcome to Address Book Problem Using JS");
+
+class Contact {
+    constructor(...params) {
+        this.firstName = params[0];
+        this.lastName = params[1];
+        this.address = params[2];
+        this.city = params[3];
+        this.state = params[4];
+        this.zip = params[5];
+        this.phoneNo = params[6];
+        this.email = params[7];
+    }
+}
